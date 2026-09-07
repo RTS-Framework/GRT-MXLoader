86ff0133e5a2ffe617f454f28422f096e19679a4
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-09-07 13:45:27 +0800

    add command for clean the dotnet project in the builder directory.

--- Clean.bat
+++ Clean.bat
@@ -12,6 +12,11 @@ rd /S /Q "builder\cs-beacon\Release"
 rd /S /Q "builder\cs-beacon\x64"
 rd /S /Q "builder\cs-beacon\x86"
 
+rd /S /Q "builder\dotnet\Debug"
+rd /S /Q "builder\dotnet\Release"
+rd /S /Q "builder\dotnet\x64"
+rd /S /Q "builder\dotnet\x86"
+
 rd /S /Q "tool\test_stage\Debug"
 rd /S /Q "tool\test_stage\Release"
 rd /S /Q "tool\test_stage\x64"
