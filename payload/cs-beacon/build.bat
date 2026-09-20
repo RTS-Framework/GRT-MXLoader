d6bd5c5b3594b04146a1c9d03819ae4dded7ddf9
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-09-20 15:29:42 +0800

    cs-beacon/build.bat: rename the project, update comment about fake stage.

--- payload/cs-beacon/build.bat
+++ payload/cs-beacon/build.bat
@@ -7,13 +7,13 @@ if "%VisualStudio%" == "" (
 )
 call "%VisualStudio%\VC\Auxiliary\Build\vcvars64.bat"
 
-echo ================== generate test stage ===================
-MSBuild.exe ..\..\GRT-MXLoader.sln /t:payload/fake_stage /p:Configuration=Release /p:Platform=x86
-MSBuild.exe ..\..\GRT-MXLoader.sln /t:payload/fake_stage /p:Configuration=Release /p:Platform=x64
+echo ================== generate fake stage ===================
+MSBuild.exe ..\..\GRT-MXLoader.sln /t:payload\cs-beacon /p:Configuration=Release /p:Platform=x86
+MSBuild.exe ..\..\GRT-MXLoader.sln /t:payload\cs-beacon /p:Configuration=Release /p:Platform=x64
 
-echo ==================== copy test stage =====================
-copy /Y ..\..\Release\test_stage.dll     ..\..\loader\cs-beacon\testdata\stage_x86.dat
-copy /Y ..\..\x64\Release\test_stage.dll ..\..\loader\cs-beacon\testdata\stage_x64.dat
+echo ==================== copy fake stage =====================
+copy /Y ..\..\Release\cs-beacon.dll     ..\..\loader\cs-beacon\testdata\stage_x86.dat
+copy /Y ..\..\x64\Release\cs-beacon.dll ..\..\loader\cs-beacon\testdata\stage_x64.dat
 
 echo =================== clean build files ====================
 rd /S /Q "Release"
@@ -22,6 +22,6 @@ rd /S /Q "..\..\Release"
 rd /S /Q "..\..\x64"
 
 echo ==========================================================
-echo              build test beacon stage finish!
+echo              build fake beacon stage finish!
 echo ==========================================================
 pause
