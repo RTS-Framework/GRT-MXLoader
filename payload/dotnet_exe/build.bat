f7ef74cf61db05336ef6c537027f3961db4c4f81
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-09-21 14:51:12 +0800

    dotnet_exe/build.bat: add script for build the dotnet exe image for test.

---
+++ payload/dotnet_exe/build.bat
@@ -0,0 +1,25 @@
+@echo off
+
+echo ========== initialize Visual Studio environment ==========
+if "%VisualStudio%" == "" (
+    echo environment variable "VisualStudio" is not set
+    exit /b 1
+)
+call "%VisualStudio%\VC\Auxiliary\Build\vcvars64.bat"
+
+echo ================== generate dotnet exe ===================
+MSBuild.exe ..\..\GRT-MXLoader.sln /t:payload\dotnet_exe /p:Configuration=Release /p:Platform="Any CPU"
+
+echo ==================== copy dotnet exe =====================
+copy /Y bin\Release\dotnet_exe.exe ..\..\loader\dotnet\testdata\dotnet_exe.dat
+
+echo =================== clean build files ====================
+rd /S /Q "bin"
+rd /S /Q "obj"
+rd /S /Q "..\..\Release"
+rd /S /Q "..\..\x64"
+
+echo ==========================================================
+echo                  build dotnet exe finish!
+echo ==========================================================
+pause
