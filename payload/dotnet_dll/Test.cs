405217a27a2f57ffc2e218521e32b3bc8b2b1f58
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-09-23 13:15:55 +0800

    dotnet_dll/Test.cs: implement the dotnet dll image about method caller.

--- payload/dotnet_dll/Test.cs
+++ payload/dotnet_dll/Test.cs
@@ -1,6 +1,29 @@
-﻿namespace dotnet_dll
+﻿using System;
+
+namespace dotnet_dll
 {
-    public class Class1
+    public class Test
     {
+        public void Method0()
+        {
+            Console.WriteLine("Method0 is called");
+            return;
+        }
+
+        public int Method1(string arg0, string arg1)
+        {
+            Console.WriteLine("Method1 is called");
+            if (arg0 != "arg0")
+            {
+                Console.WriteLine("invalid argument 0");
+                return 1;
+            }
+            if (arg1 != "arg1")
+            {
+                Console.WriteLine("invalid argument 1");
+                return 1;
+            }
+            return 0;
+        }
     }
 }
