5eea43efc0d57b05b1dd41bad983b93d33941321
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-09-23 13:14:57 +0800

    dotnet_exe/Program.cs: implement the dotnet exe image about test command line.

--- payload/dotnet_exe/Program.cs
+++ payload/dotnet_exe/Program.cs
@@ -1,9 +1,44 @@
-﻿namespace dotnet
+﻿using System;
+
+namespace dotnet_exe
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            if (args == null || args.Length == 0)
+            {
+                Console.WriteLine("PASS: no arguments");
+                return 0;
+            }
+
+            if (args.Length != 4)
+            {
+                Console.WriteLine("invalid argument count");
+                return 1;
+            }
+
+            if (args[0] != "-p1")
+            {
+                Console.WriteLine("invalid argument: 0");
+                return 2;
+            }
+            if (args[1] != "123")
+            {
+                Console.WriteLine("invalid argument: 1");
+                return 2;
+            }
+            if (args[2] != "-p2")
+            {
+                Console.WriteLine("invalid argument: 2");
+                return 2;
+            }
+            if (args[3] != "abc")
+            {
+                Console.WriteLine("invalid argument: 3");
+                return 2;
+            }
+            return 0;
         }
     }
 }
