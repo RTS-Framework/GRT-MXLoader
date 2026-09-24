c22dac96015da4d619955ad79d7450992d9427b6
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-09-24 13:45:16 +0800

    dotnet/boot.h: add more config fields for dotnet dll image.

--- boot/dotnet/boot.h
+++ boot/dotnet/boot.h
@@ -9,7 +9,10 @@
 
 #define ARG_ID_PE_IMAGE  1
 #define ARG_ID_CMDLINE   2
-#define ARG_ID_WAIT      3
+#define ARG_ID_CLASS     3
+#define ARG_ID_METHOD    4
+#define ARG_ID_ARGUMENT  5
+#define ARG_ID_WAIT      6
 
 #define EMBED_ENABLE_COMPRESSION  1
 #define EMBED_DISABLE_COMPRESSION 0
@@ -17,20 +20,26 @@
 #define ERR_NOT_FOUND_PE_IMAGE       0x7F00FF01
 #define ERR_EMPTY_PE_IMAGE_DATA      0x7F00FF02
 #define ERR_NOT_FOUND_CMDLINE        0x7F00FF03
-#define ERR_NOT_FOUND_WAIT           0x7F00FF04
-#define ERR_INVALID_WAIT             0x7F00FF05
-#define ERR_INVALID_LOAD_MODE        0x7F00FF06
-#define ERR_INVALID_EMBED_CONFIG     0x7F00FF07
-#define ERR_INVALID_COMPRESS_DATA    0x7F00FF08
-#define ERR_INVALID_PE_IMAGE         0x7F00FF09
-#define ERR_INVALID_HTTP_CONFIG      0x7F00FF0A
-#define ERR_INVALID_HTTP_STATUS_CODE 0x7F00FF0B
+#define ERR_NOT_FOUND_CLASS          0x7F00FF04
+#define ERR_NOT_FOUND_METHOD         0x7F00FF05
+#define ERR_NOT_FOUND_ARGUMENT       0x7F00FF06
+#define ERR_NOT_FOUND_WAIT           0x7F00FF07
+#define ERR_INVALID_WAIT             0x7F00FF08
+#define ERR_INVALID_LOAD_MODE        0x7F00FF09
+#define ERR_INVALID_EMBED_CONFIG     0x7F00FF0A
+#define ERR_INVALID_COMPRESS_DATA    0x7F00FF0B
+#define ERR_INVALID_PE_IMAGE         0x7F00FF0C
+#define ERR_INVALID_HTTP_CONFIG      0x7F00FF0D
+#define ERR_INVALID_HTTP_STATUS_CODE 0x7F00FF0E
 
 #define CTX_TYPE_TEST 0x01
 
 typedef struct {
     void* Image;
     void* CommandLine;
+    void* Class;
+    void* Method;
+    void* Argument;
     BOOL  Wait;
 } Config;
 
@@ -38,6 +47,9 @@ typedef struct {
     uint  Type;
     void* Image;
     void* CommandLine;
+    void* Class;
+    void* Method;
+    void* Argument;
     void* Runtime;
 } CTX_Test;
 
